7b89dbc78f02e61840c56cba2b2282934d837f6f
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-16 11:06:54 -0600

    init basic slash command export

--- src/commands/info/status.ts
+++ src/commands/info/status.ts
@@ -1,3 +1,12 @@
-export const Status = () => {
-  return (console.log('status'))
+import { SlashCommandBuilder } from '@discordjs/builders'
+import type{ Command } from '../../interfaces/Command'
+
+export const status: Command = {
+  data: new SlashCommandBuilder()
+  .setName('status')
+  .setDescription(`Ask Quebert what hes got going on behind the scenes`),
+  run: async (interaction) => {
+    await interaction.reply({content: `Honestly... feeling good, as you can see looking good, ready to go!`, ephemeral: true })
+  }
+  
 }
