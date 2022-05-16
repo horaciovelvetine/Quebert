bbf5f6bd7c253cc45baecfd19dd79dc7d91a8fb6
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-16 11:24:01 -0600

    misc build to get commands working

--- dist/index.js
+++ dist/index.js
@@ -5,34 +5,27 @@ const discord_js_1 = require("discord.js");
 const config_1 = tslib_1.__importDefault(require("./config"));
 const index_1 = require("./commands/index");
 const { token } = config_1.default;
-let COMMANDS = new discord_js_1.Collection();
+let SlashCommands = [];
 const client = new discord_js_1.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"],
     presence: {
         status: 'online',
         activities: [{
-                name: `@me for helpful info`,
+                name: `@me for info!`,
                 type: 'LISTENING'
             }]
     },
 });
-(0, index_1.DeployCommands)();
-client.on('ready', () => {
+client.on('ready', async () => {
+    SlashCommands = await (0, index_1.DeployCommands)();
     console.log(`Quebert is Logged in and ready, use (ctrl + c) to end this process.`);
 });
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand())
-        return;
-    let command = COMMANDS.get(interaction.commandName);
-    if (!command)
-        return;
-    try {
-        await command.execute(interaction);
-    }
-    catch (err) {
-        console.error(err);
-        await interaction.reply({ content: 'Something went wrong, try again...', ephemeral: true });
-    }
+    console.log(interaction);
+    console.log(SlashCommands);
+    // if (interaction.isCommand()) {
+    //   for (const Command of allSlashCommands)
+    // }
 });
 client.login(token);
 //# sourceMappingURL=index.js.map
