c647c4d61aa878d0c36462aac889c46259ff0fa7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-28 12:03:13 -0600

    fix typing issue with the add of subcommands
    can't remember if the misdirect on syntax came from guide or a tutorial i found online...

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,7 +1,7 @@
-import type { SlashCommandBuilder } from '@discordjs/builders'
+import type { SlashCommandSubcommandsOnlyBuilder, SlashCommandBuilder } from '@discordjs/builders'
 import type { CommandInteraction } from 'discord.js'
 
 export interface Command {
-  data: Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand">;
+  data: SlashCommandSubcommandsOnlyBuilder | SlashCommandBuilder;
   run: (interaction: CommandInteraction) => Promise<void>;
 }
