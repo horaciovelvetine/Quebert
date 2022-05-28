da99dbccd518d1df8e22dbd7c8b4a946a57b4d39
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-27 22:13:22 -0600

    add structure for initial configuration

--- src/utils/dev/DeployCommands.ts
+++ src/utils/dev/DeployCommands.ts
@@ -1,5 +1,26 @@
+import config from './config'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+import { sendAlertToConsole } from './sendAlertToConsole'
+
 import type { Command } from '../../interfaces/Command'
 
-export const DeployCommands = (): Command[] => {
-  return([])
+const {token, client, guild} = config;
+
+const rest = new REST({ version: '9' }).setToken(token!);
+
+export const DeployCommands = async (): Promise<Command[]> => {
+  try {
+    sendAlertToConsole(`Updating Quebert's application (/) commands. Brb.`)
+    await rest.put(
+      Routes.applicationGuildCommands(client!, guild!),
+      { body: ['see below']}
+      //! { body: AllSlashCommands.map(c => c.data.toJSON()) },
+    )
+    sendAlertToConsole(`Im back and successfully updated Quebert's application (/) commands!`)
+    return ([]) //! should be an array of commands, but this may be optional
+  } catch (error) {
+    console.error(error)
+  }
+  return([]) //! Same here as above
 }
