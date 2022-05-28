f1cb35bf29681596ee4fb5fa42e61b1e313c2c5c
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-28 11:56:12 -0600

    init command w/ subcommand structure
    has typing issue against the command 'DATA' that was def in interfaces
    
    needs further sorting

---
+++ src/commands/que/clear.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+//types
+import type { CommandInteraction } from 'discord.js'
+import type { Command } from "../../interfaces/_index";
+
+export const clear: Command = {
+  data: new SlashCommandBuilder()
+    .setName('clear')
+    .setDescription('Remove posts from the Queue by: ID, Last Added, or All')
+    .addSubcommand(subCommand =>
+      subCommand.setName('ID')
+        .setDescription('Remove post by ID')
+        .addUserOption(opt =>
+          opt.setName('ID'))
+        .setDescription('The Posts ID'))
+    .addSubcommand(subCommand =>
+      subCommand.setName('Last')
+        .setDescription('Remove the last post added to the Queue'))
+    .addSubcommand(subCommand =>
+      subCommand.setName('All')
+        .setDescription('[CAUTION] Removes all posts from the Queue')),
+  run: async (interaction: CommandInteraction) => {
+    // TODO: move towards this logic, ephemeral reply was causing typing issues
+    // await interaction.client.guilds.filter(guild => guild.id === ENV['MOD_ONLY']).send({content: 'Deleted some stuff'})
+    //! This is really just a workaround, it may even 
+    await interaction.followUp('Hey Im following up with this, I deleted everything')
+  }
+}
