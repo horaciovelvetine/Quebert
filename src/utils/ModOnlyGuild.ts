2007c997274ca22d622b938f32de87fa3c114175
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-05-28 15:40:28 -0600

    clunky first modonly helper.
    TODO still need to find a way around this Any Channel conundrum
    
    Might be time to ping @LukeTillman again lol

---
+++ src/utils/ModOnlyGuild.ts
@@ -0,0 +1,15 @@
+import config from './dev/config'
+import type { CommandInteraction } from "discord.js";
+
+let { mod } = config
+
+export const ModOnlyGuild = (interaction: CommandInteraction) => {
+  let chan = interaction.client.channels.cache.get(mod)!
+
+  if (chan.isText()) {
+    return (chan)
+  }
+  //TODO This is a clunky solution for this. Need to keep moving already 2+ hours this is the best solution so far
+  console.log('Channel found is not of the text type')
+
+}
