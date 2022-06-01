d1a9d361118b54a3839c634db71d1a37d1e40688
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-01 13:48:00 -0600

    uses as aliasing to fix TextChannel typing needs

--- src/utils/ModOnlyGuild.ts
+++ src/utils/ModOnlyGuild.ts
@@ -1,14 +1,8 @@
 import config from './dev/config';
-import type { CommandInteraction } from 'discord.js';
+import type { CommandInteraction, TextChannel } from 'discord.js';
 
 let { mod } = config;
 
 export const ModOnlyGuild = (interaction: CommandInteraction) => {
-	let chan = interaction.client.channels.cache.get(mod)!;
-
-	if (chan.isText()) {
-		return chan;
-	}
-	//TODO This is a clunky solution for this. Need to keep moving already 2+ hours this is the best solution so far
-	console.log('Channel found is not of the text type');
+	return interaction.client.channels.cache.get(mod) as unknown as TextChannel;
 };
