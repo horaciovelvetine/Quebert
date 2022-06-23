006edd1c218ffaac471f10bdf4d03903aea917ff
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-23 17:09:30 -0600

    simplify on refactor, ideate ohn start stop func, setup for testing

--- src/jobs/_queRoutine.ts
+++ src/jobs/_queRoutine.ts
@@ -4,42 +4,51 @@ import { SimpleIntervalJob, AsyncTask, ToadScheduler } from 'toad-scheduler';
 import { baseUrlFormatter } from '../api';
 import config from '../config/config';
 import type { APIResponseInt } from '../interfaces';
-import { devConsoleMessage, queRoutineFailEmbed } from '../messages';
-import { queRoutineSuccessEmbed } from '../messages/embeds/_queRoutineSuccess';
+import { devConsoleMessage, queRoutineFailEmbed, queRoutineSuccessEmbed } from '../messages';
 
 let { mod } = config;
+const ID = 'QueRoutine';
 
-// "".stopById('QueRoutine')
-// "".removeById('QueRoutine')
-// "".getById('QueRoutine).getStatus() === 'running' | 'stopped'
-
-export const initQueRoutine = async (guilds: Collection<string, TextChannel>, queScheduler: ToadScheduler) => {
-	let postFromQueue = new AsyncTask(
-		'post-from-queue',
+export const initQueRoutine = async (
+	guilds: Collection<string, TextChannel>,
+	scheduler: ToadScheduler
+	// cronfig = [0, 0, 15, 0, 0, true] => potential for later setup when start/stoppable
+) => {
+	let task = new AsyncTask(
+		ID,
 		async () => {
-			let currentQueue: APIResponseInt = await axios
+			let queRoutine: APIResponseInt = await axios
 				.get(baseUrlFormatter('/post-routine'))
-				.then((response) => response.data)
-				.catch((error) => {
-					devConsoleMessage(error);
-					guilds.get(mod)!.send({ embeds: [queRoutineFailEmbed(currentQueue.message)] });
-				});
-			//! SEND ITERATION IS LAST STEP
-			console.log(currentQueue.message);
-			// currentQueue.payload.posts.forEach((post) => {
-			// 	guilds.get(post.target_id)?.send({ content: post.body });
-			// });
-
-			guilds.get(mod)!.send({ embeds: [queRoutineSuccessEmbed(currentQueue.message)] });
+				.then((response) => response.data);
+			
+			
+			//! START::
+			//? should loop over response, and get each guild in payload and 
+			devConsoleMessage(queRoutine.message);
+			guilds.get(mod)!.send({ embeds: [queRoutineSuccessEmbed(queRoutine.message)] });
 		},
 		(error) => {
 			devConsoleMessage(error.message);
-			guilds.get(mod)!.send({
-				content: `Encountered an unknown issue during the QueRoutine, please restart Quebert and try again, message: ${error}`,
-			});
+			guilds.get(mod)!.send({ embeds: [queRoutineFailEmbed(error.message)] });
 		}
 	);
 
-	let QueRoutine = new SimpleIntervalJob({ minutes: 15, runImmediately: true }, postFromQueue, 'QueRoutine');
-	queScheduler.addSimpleIntervalJob(QueRoutine);
+	let queRoutine = new SimpleIntervalJob(
+		{
+			minutes: 3,
+			runImmediately: true,
+		},
+		task,
+		ID
+	);
+	scheduler.addSimpleIntervalJob(queRoutine);
+};
+
+export const stopQueRoutine = (scheduler: ToadScheduler) => {
+	pauseQueRoutine(scheduler);
+	scheduler.removeById(ID);
+};
+
+export const pauseQueRoutine = (scheduler: ToadScheduler) => {
+	scheduler.stopById(ID);
 };
