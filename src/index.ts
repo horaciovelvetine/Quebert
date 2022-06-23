2e84cd16ee3561ca5584d280317a2f4e76d81f87
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-23 17:08:36 -0600

    remove T/C up a level in refactor, now less error prone, refactor

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 // config & lib
 import config, { clientDetails } from './config/config';
-import { Client, Collection, Interaction, TextChannel } from 'discord.js';
+import type { Collection, Interaction, TextChannel } from 'discord.js';
+import { Client } from 'discord.js';
 import { deploySlashCommands } from './config';
 import { ToadScheduler } from 'toad-scheduler';
 import type { CombinedCommandsInt } from './interfaces';
 import { initQueRoutine } from './jobs';
 
-
 const { token } = config;
 const queScheduler = new ToadScheduler();
 
-const client: Client = new Client(clientDetails());
-
+const client = new Client(clientDetails());
 
 let slashCommands: CombinedCommandsInt[];
 let guilds: Collection<string, TextChannel>;
@@ -22,21 +21,22 @@ client.on('ready', async () => {
 		TextChannel
 	>;
 	slashCommands = await deploySlashCommands();
-	initQueRoutine(guilds, queScheduler)
+	initQueRoutine(guilds, queScheduler);
 	console.log(`Quebert is Logged in and ready`);
 });
 
 client.on('interactionCreate', async (interaction: Interaction) => {
-	if (interaction.isCommand()) {
-		for (const Command of slashCommands) {
-			if (interaction.commandName === Command.data.name) {
-				try {
-					await Command.run(interaction);
-				} catch (err) {
-					interaction.reply({ content: `${err}`, ephemeral: true });
-				}
+	if (!interaction.isCommand()) return;
+
+	try {
+		for (const command of slashCommands) {
+			if (interaction.commandName === command.data.name) {
+				await command.run(interaction);
 			}
 		}
+	} catch (error) {
+		console.log(error);
+		await interaction.reply({ content: `${error}`, ephemeral: true });
 	}
 });
 
