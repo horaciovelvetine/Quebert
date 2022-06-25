250f47558db35e914b3aebe6121bcb329a0d5dd3
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-25 17:04:28 -0600

    remove errant extra fields, just que length and time to next

--- src/messages/embeds/_status.ts
+++ src/messages/embeds/_status.ts
@@ -4,10 +4,8 @@ import type { APIResponseInt } from '../../interfaces';
 export const statusEmbed = (interaction: CommandInteraction, response: APIResponseInt) => {
 	return new MessageEmbed()
 		.addFields(
-			{ name: 'Posts Cumulative:', value: response.payload.total_posts! },
-			{ name: 'Number of posts in queue:', value: response.queue_length! },
-			{ name: 'Time to next post:', value: response.payload.time_to_next_post! },
-			{ name: 'Time until queue empty:', value: response.payload.time_to_empty! }
+			{ name: 'Current queue length:', value: response.payload.total_queued! },
+			{ name: 'Time to next post:', value: response.payload.time_to_next! }
 		)
 		.setTitle(`Quebert Status Info:`)
 		.setAuthor({ name: `${interaction.user.username}` });
