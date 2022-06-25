df13e1ca632ca13cc2ca0aeacc27773b72641613
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-24 22:40:06 -0600

    shorten up messaging, overall prettier easy to read

--- src/commands/_que.ts
+++ src/commands/_que.ts
@@ -9,11 +9,9 @@ import type { SlashCommandInt } from '../interfaces';
 export const que: SlashCommandInt = {
 	data: new SlashCommandBuilder()
 		.setName('que')
-		.setDescription('Add a message to the Queue for Quebert to send later.')
-		.addChannelOption((opt) =>
-			opt.setName('target-channel').setDescription('Channel where Quebert will send the message').setRequired(true)
-		)
-		.addStringOption((opt) => opt.setName('msg-body').setDescription('The body of thee message').setRequired(true)),
+		.setDescription('Add a message to the Queue')
+		.addChannelOption((opt) => opt.setName('target-channel').setDescription('Channel target').setRequired(true))
+		.addStringOption((opt) => opt.setName('msg-body').setDescription('Message body').setRequired(true)),
 	run: async (interaction: CommandInteraction) => {
 		let targetGuild = interaction.options.getChannel('target-channel')!;
 		let msgBody = interaction.options.getString('msg-body')!;
@@ -23,11 +21,12 @@ export const que: SlashCommandInt = {
 			payload: {
 				id: interaction.id,
 				body: msgBody,
-				target_channel: targetGuild,
+				target: targetGuild,
 			},
 		});
-		let modEmbedPreview = addQueEmbed(interaction, response);
-		modOnlyGuild(interaction)?.send({ embeds: [modEmbedPreview] });
+
+
+		modOnlyGuild(interaction)?.send({ embeds: [addQueEmbed(interaction, response.payload)] });
 		await interaction.reply({ content: `${response.message}`, ephemeral: true });
 	},
 };
