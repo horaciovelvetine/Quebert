62b67b30027020a964405d1eac5c287c0867ed99
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-06-27 22:18:05 -0600

    add the status of the current QueRoutine taks

--- src/commands/_status.ts
+++ src/commands/_status.ts
@@ -6,10 +6,14 @@ import type { SlashCommandInt } from '../interfaces';
 
 export const status: SlashCommandInt = {
 	data: new SlashCommandBuilder().setName('status').setDescription(`Ask Quebert how it's going`),
-	run: async (interaction) => {
+	run: async (interaction, queScheduler) => {
 		let statusResponse = await postSlashCommand({ command: 'status', payload: 'default' });
-
+		let queRoutineStatus = queScheduler.getById('QueRoutine').getStatus();
 		let modEmbedPreview = statusEmbed(interaction, statusResponse);
-		await interaction.reply({ embeds: [modEmbedPreview], ephemeral: true });
+		await interaction.reply({
+			embeds: [modEmbedPreview],
+			ephemeral: true,
+			content: `Que Routine is currently: ${queRoutineStatus}`,
+		});
 	},
 };
