626f0dbf85cebea60a4606c0eed9ad8f2984df9b
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-07-26 14:29:20 -0600

    now imports all commands and exports single AllSlashCommands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,8 @@
-export { status } from './_status';
-export { que } from './_que';
-export { clearLast, clearPost } from './clear';
-export { setCronfig, currentCronfig } from './cronfig';
-export { pauseQueRoutine, startQueRoutine, stopQueRoutine } from './queRoutine';
+import { status } from './_status';
+import { que } from './_que';
+import { clearLast, clearPost } from './clear';
+import { setCronfig, currentCronfig } from './cronfig';
+import { pauseQueRoutine, startQueRoutine, stopQueRoutine } from './queRoutine';
+
+
+export const AllSlashCommands = [status, que, clearLast, clearPost, setCronfig, currentCronfig, pauseQueRoutine, startQueRoutine, stopQueRoutine];
