2a6066b1288f11c87f58246be886e9ea593e5b23
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-07-26 17:43:10 -0600

    queRoutine job init, seperate task and initialization for cleanlieness

--- src/jobs/que_routine/_createTask.ts
+++ src/jobs/que_routine/_createTask.ts
@@ -1,17 +1,61 @@
-import { AsyncTask } from "toad-scheduler";
-import { getQueRoutine } from "../../api";
-import { devConsoleMessage } from "../../messages";
+// external libs
+import type { Client, TextChannel } from 'discord.js';
+import { AsyncTask, ToadScheduler } from 'toad-scheduler';
 
-const TASK_ID = 'QueRoutine';
+// lib
+import { getQueRoutine } from '../../api';
+import { devConsoleMessage, queRoutineFailEmbed, queRoutineSuccessEmbed } from '../../messages';
+import { getModOnlyFromClient } from '../../utils';
 
-export const createQueRoutineTask = async () => {
-  return new AsyncTask(
-    TASK_ID,
-    async () => {
-      let payload = getQueRoutine()
-    },
-    (error) => {
-      devConsoleMessage(error.message)
-    }
-  )
+interface CREATE_QUE_ROUTINE_PROPS {
+	client: Client;
+	scheduler: ToadScheduler;
+	task_id: 'QueRoutine';
 }
+
+export const createQueRoutineTask = ({ client, scheduler, task_id }: CREATE_QUE_ROUTINE_PROPS): AsyncTask => {
+	return new AsyncTask(
+		task_id,
+		async () => {
+			let response = await getQueRoutine();
+			let payload = response.payload;
+
+			switch (true) {
+				// no posts in Queue: success path, stop task
+				case payload.posts.length === 0:
+					if (scheduler.getById(task_id).getStatus()) {
+						// task does exist, and is paused since no posts
+						scheduler.getById(task_id).stop();
+						getModOnlyFromClient(client).send({
+							embeds: [queRoutineSuccessEmbed(response.message)],
+							content: 'Queue Paused: add items queue with: `/que`',
+						});
+					} else {
+						// task does not currently exist and will need to be created/started
+						getModOnlyFromClient(client).send({
+							content: 'No Queue task is currently running start one by using `/start-que-routine`',
+						});
+					}
+					break;
+				// posts in Queue: success path, send posts
+				case payload.posts.length >= 1:
+					payload.posts.forEach((post) => {
+						let targetChannel = client.channels.cache.get(post.target) as unknown as TextChannel;
+						targetChannel.send(post.body);
+					});
+					// send success message to moderator_only channel
+					getModOnlyFromClient(client).send({ embeds: [queRoutineSuccessEmbed(response.message)] });
+					break;
+
+				default:
+					break;
+			}
+			//sends notice to console
+			devConsoleMessage(response.message);
+		},
+		(error) => {
+			devConsoleMessage(error.message);
+			getModOnlyFromClient(client).send({ embeds: [queRoutineFailEmbed(error)] });
+		}
+	);
+};
