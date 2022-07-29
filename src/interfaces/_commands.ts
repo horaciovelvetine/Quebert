48a2c929314625fb9b50753e23d46ff521fdab57
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2022-07-28 18:10:17 -0600

    more explicit naming for JobsSchedulerClient

--- src/interfaces/_commands.ts
+++ src/interfaces/_commands.ts
@@ -4,12 +4,12 @@ import type { ToadScheduler } from 'toad-scheduler';
 
 export interface SLASH_COMMAND {
 	data: Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>;
-	run: (interaction: CommandInteraction<CacheType>, jobsSchedulerClient?: ToadScheduler) => Promise<void>;
+	run: (interaction: CommandInteraction<CacheType>, jobsSchedulerClient: ToadScheduler) => Promise<void>;
 }
 
 export interface SLASH_W_SUB_COMMANDS {
 	data: SlashCommandSubcommandsOnlyBuilder | SlashCommandBuilder;
-	run: (interaction: CommandInteraction<CacheType>, jobsSchedulerClient?: ToadScheduler) => Promise<void>;
+	run: (interaction: CommandInteraction<CacheType>, jobsSchedulerClient: ToadScheduler) => Promise<void>;
 }
 
 export type COMBINED_COMMANDS = SLASH_COMMAND | SLASH_W_SUB_COMMANDS;
